560a489a8e46d6af8f4d216f7c5092b236a00bcd
Olena Rostotskyy <olenazachkow@gmail.com>
2019-04-03 20:51:54 -0700

    react route

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React, { Component } from 'react';
-
-
 import Main from './components/MainComponents';
 import './App.css';
+import { BrowserRouter } from 'react-router-dom';
+
 
 
 class App extends Component {
   
   render() {
     return (
-      <div>
-
-<Main />
-        </div>
-         
+      <BrowserRouter>
+      <div className="App">
+        <Main />
+      </div>
+    </BrowserRouter>  
     );
   }
 }
--- src/components/MainComponents.js
+++ src/components/MainComponents.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import Home from './HomeComponent';
 import Menu from './MenuComponents';
 import DishDetail from './dishdetailComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import {DISHES} from '../shared/dishes';
+import { Switch, Route, Redirect } from 'react-router-dom';
+
 
 class Main extends Component {
   constructor(props) {
@@ -11,31 +14,36 @@ class Main extends Component {
 
     this.state = {
       dishes: DISHES,
-      selectedDish: null //new property/ var
-    }
+    };
   }
 
-  onDishSelect(dishId) {
-    this.setState({ selectedDish: dishId});// when we need to change the state, we need to use the this.setState function call.
-}
-
   render() {
-    return (
+      const HomePage = () => {
+        return(
+            <Home />
+        );
+      }
+      return (
       <div>
 <Header/>
-<Menu dishes={this.state.dishes} //onClick when it is invoked, will pass the dishId parameter, which will come in as the parameter here for the onDishselect
-onClick={(dishId) => this.onDishSelect(dishId)}/> 
+<Switch>
+  
+              <Route path='/home' component={HomePage} />
+              
+            <Route exact path='/menu' render={() => <Menu dishes={this.state.dishes} />} />
 
-
-<DishDetail //For each dish, what I'm going to do, is to check dishId is this the same as this state selected dish
-dish={this.state.dishes.filter((dish)=>dish.id===this.state.selectedDish)[0]} />
+              <Redirect to="/home" />  
+              
+          </Switch>
 <Footer/>
         </div>//from this dishes array, I'm going to select out one dish, that one particular dish is the one for which the dishId matches the selected dish here. 
        //filter function, if you recall from your knowledge of JavaScript this filter function will give the sub array of the dishes for which the sub-array contains are rather, constrained part of the array, or just the elements from the array, for which this property, the dishId matches selected dish.  
    //[0]=first item in arrey
-       );
+      );
 
   }
 }
+//anything that doesn't match home or menu will be redirect to home for the moment. 
+
 
 export default Main;
