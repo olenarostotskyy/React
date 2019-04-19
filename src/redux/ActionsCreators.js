7781cbb217b7e04e47102a061df6250dbb7edc72
Olena Rostotskyy <olenazachkow@gmail.com>
2019-04-18 22:01:37 -0700

    fetch handling errors

--- src/redux/ActionsCreators.js
+++ src/redux/ActionsCreators.js
@@ -2,6 +2,7 @@ import * as ActionTypes from './ActionTypes';
 import { DISHES } from '../shared/dishes';
 import { baseUrl } from '../shared/baseUrl';
 
+
 export const addComment = (dishId, rating, author, comment) => ({
     type: ActionTypes.ADD_COMMENT,
     payload: {
@@ -12,18 +13,31 @@ export const addComment = (dishId, rating, author, comment) => ({
     }
 });
 
-
-
-
-export const fetchDishes = () => (dispatch) => {    
+export const fetchDishes = () => (dispatch) => {
     dispatch(dishesLoading(true));
-    return fetch(baseUrl+'dishes')
-    .then(response => response.json())
-    .then(dishes => dispatch(addDishes(dishes)));  
+
+    return fetch(baseUrl + 'dishes')
+        .then(response => {
+            if (response.ok) {// this is how you handle response from the server
+                return response;
+            }
+            else {
+                var error = new Error('Error ' + response.status + ': ' + response.statusText);
+                error.response = response;
+                throw error;
+            }
+        },
+            error => {//error hendler
+                var errmess = new Error(error.message);
+                throw errmess;
+            })
+        .then(response => response.json())
+        .then(dishes => dispatch(addDishes(dishes)))
+        .catch(error => dispatch(dishesFailed(error.message)));  //dishesfailed action with the erroe message
 }
 
-export const dishesLoading =()=>({
-    type:ActionTypes.DISHES_LOADING
+export const dishesLoading = () => ({
+    type: ActionTypes.DISHES_LOADING
 });
 
 export const dishesFailed = (errmess) => ({
@@ -34,13 +48,30 @@ export const dishesFailed = (errmess) => ({
 export const addDishes = (dishes) => ({// function that return an action
     type: ActionTypes.ADD_DISHES,
     payload: dishes
-}); 
+});
 
 
-export const fetchComments = () => (dispatch) => {    
+export const fetchComments = () => (dispatch) => {
     return fetch(baseUrl + 'comments')
-    .then(response => response.json())
-    .then(comments => dispatch(addComments(comments)));
+        .then(response => {
+            if (response.ok) {// this is how you handle response from the server
+                return response;
+            }
+            else {
+                var error = new Error('Error ' + response.status + ': ' + response.statusText);
+                error.response = response;
+                throw error;
+            }
+        },
+            error => {//error hendler
+                var errmess = new Error(error.message);
+                throw errmess;
+            })
+
+        .then(response => response.json())
+        .then(comments => dispatch(addComments(comments)))
+        .catch(error => dispatch(commentsFailed(error.message)));  //dishesfailed action with the erroe message
+
 };
 
 export const commentsFailed = (errmess) => ({
@@ -54,27 +85,43 @@ export const addComments = (comments) => ({
 });
 
 export const fetchPromos = () => (dispatch) => {
-    
+
     dispatch(promosLoading());
 
     return fetch(baseUrl + 'promotions')
-    .then(response => response.json())
-    .then(promos => dispatch(addPromos(promos)));
-}
+        .then(response => {
+            if (response.ok) {// this is how you handle response from the server
+                return response;
+            }
+            else {
+                var error = new Error('Error ' + response.status + ': ' + response.statusText);
+                error.response = response;
+                throw error;
+            }
+        },
+            error => {//error hendler
+                var errmess = new Error(error.message);
+                throw errmess
+            })
+                    .then(response => response.json())
+                    .then(promos => dispatch(addPromos(promos)))
+                    .catch(error => dispatch(promosFailed(error.message)));  //dishesfailed action with the erroe message
+            }
+
 
 export const promosLoading = () => ({
-    type: ActionTypes.PROMOS_LOADING
-});
+        type: ActionTypes.PROMOS_LOADING
+    });
 
-export const promosFailed = (errmess) => ({
-    type: ActionTypes.PROMOS_FAILED,
-    payload: errmess
-});
+    export const promosFailed = (errmess) => ({
+        type: ActionTypes.PROMOS_FAILED,
+        payload: errmess
+    });
 
-export const addPromos = (promos) => ({
-    type: ActionTypes.ADD_PROMOS,
-    payload: promos
-});
+    export const addPromos = (promos) => ({
+        type: ActionTypes.ADD_PROMOS,
+        payload: promos
+    });
 
 
 
